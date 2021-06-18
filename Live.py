b1d930d44fbb2198bfdfafdb9e075f5cfdfb2111
Adomet <ahmedtalhabudak@gmail.com>
2021-06-18 19:46:56 +0300

    Stratv8 param update

--- Live.py
+++ Live.py
@@ -167,7 +167,7 @@ def main():
     cerebro.adddata(data)
     
     # Include Strategy
-    cerebro.addstrategy(MyStratV8,537, 397, 149, 154, 171, 1124, 205, 45, -2) 
+    cerebro.addstrategy(MyStratV8,537, 397, 145, 154, 171, 1124, 205, 44, 1) 
     # Starting backtrader bot 
     initial_value = cerebro.broker.getvalue()
     log('Starting Portfolio Value: %.2f' % initial_value)
