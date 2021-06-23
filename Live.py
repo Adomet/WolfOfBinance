f49d407aecb7de67dd46d0dbd586dc29ba290365
Adomet <ahmedtalhabudak@gmail.com>
2021-06-23 11:05:14 +0300

    good luck

--- Live.py
+++ Live.py
@@ -168,7 +168,7 @@ def main():
     cerebro.adddata(data)
     
     # Include Strategy
-    cerebro.addstrategy(MyStratV8, 537, 397, 145, 154, 171, 1124, 197, 46, 1) 
+    cerebro.addstrategy(MyStratV8, 537, 397, 140, 154, 171, 1212, 207, 56, 1) 
     # Starting backtrader bot 
     initial_value = cerebro.broker.getvalue()
     log('Starting Portfolio Value: %.2f' % initial_value)
